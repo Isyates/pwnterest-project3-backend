25cc656db29d35ebb6ef0e1e1a5fd1ed74c9d455
Kyle Canamar <canamar.kyle@gmail.com>
2022-06-08 19:23:13 -0700

    creates listening server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 ////////////////////////
 // Setup - Import deps and create app object
 ////////////////////////
-
+const express = require("express")
+require("dotenv").config()
+const app = express()
+const {PORT = 4200} = process.env
 //////////////////////
 // Declare Middleware
 //////////////////////
@@ -10,7 +13,10 @@
 // Declare Routes and Routers 
 ///////////////////////
 // INDUCES - Index, New, Delete, Update, Create, Edit, Show
-
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
 ///////////////////////////
 // Server Listener
 ///////////////////////////
+app.listen(PORT, () => console.log(`Listening on port ----> ${PORT}`))
